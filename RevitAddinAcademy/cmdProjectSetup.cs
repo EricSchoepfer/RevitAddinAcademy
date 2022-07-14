e4dc1d92b0f22645da4ecff72ecccd3e285e4836
Eric Schoepfer <eric.schoepfer@gmail.com>
2022-07-13 21:54:13 -0700

    Session 3 Skills

--- RevitAddinAcademy/cmdProjectSetup.cs
+++ RevitAddinAcademy/cmdProjectSetup.cs
@@ -9,6 +9,7 @@ using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System.IO;
 using excel = Microsoft.Office.Interop.Excel;
+using Forms = System.Windows.Forms;
 
 
 #endregion
@@ -29,79 +30,125 @@ namespace RevitAddinAcademy
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            string excelFile = @"Z:\Dropbox\Dropbox\EASy Designs\Automations\Archsmarter\Revit Add-In Academy\Session02_Challenge-220706-113155.xlsx";
+            Forms.OpenFileDialog dialog = new Forms.OpenFileDialog();
+            dialog.InitialDirectory = @"C:/";
+            dialog.Multiselect = true;
+            dialog.Filter = "Revit Files | *.rvt; *.rfa";
 
+            string filePath = "";
+            if(dialog.ShowDialog() == Forms.DialogResult.OK)
             {
+                //filePath = dialog.FileName;
+                filePath = dialog.FileName;
+            }
+
+            Forms.FolderBrowserDialog folderFDialog = new Forms.FolderBrowserDialog();
+
+            string folderPath = "";
+            if(folderFDialog.ShowDialog() == Forms.DialogResult.OK)
+            {
+                folderPath = folderFDialog.SelectedPath;
+            }
+
+
+            Tuple<string, int> t1 = new Tuple<string, int>("string 1", 55);
+            Tuple<string, int> t2 = new Tuple<string, int>("string 2", 155);
+
+            TestStruct struct1;
+            struct1.Name = "Structure 1";
+                struct1.Value = 100;
+            struct1.Value2 = 10.5;
+
+            TestStruct struct2 = new TestStruct("structure 1", 10, 1004.4);
+
+
+            List<TestStruct> structlist = new List<TestStruct>();
+            structlist.Add(struct1);
+
+
+          
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(ViewFamilyType));
+
+            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(ViewFamilyType));
+
+
+
+            ViewFamilyType curVFT = null;
+            ViewFamilyType curRCPVFT = null;
+            foreach (ViewFamilyType curElem in collector)
+            {
+                if(curElem.ViewFamily == ViewFamily.FloorPlan)
                 {
-                    // Open Excel
-
-                    excel.Application excelApp = new excel.Application();
-                    excel.Workbook excelWb = excelApp.Workbooks.Open(excelFile);
-
-                    excel.Worksheet excelWsLevels = excelWb.Worksheets.Item[1];
-                    excel.Worksheet excelWsSheets = excelWb.Worksheets.Item[2];
-
-
-                    excel.Range excelRngLevels = excelWsLevels.UsedRange;
-                    excel.Range excelRngSheets = excelWsSheets.UsedRange;
-
-                    int rowCountLevels = excelRngLevels.Rows.Count;
-                    int rowCountSheets = excelRngSheets.Rows.Count;
-
-                    excelWb.Close();
-                    excelApp.Quit();
-
-
-
-
-                    using (Transaction t = new Transaction(doc))
-
-                    {
-                        t.Start("Create Some Revit Stuff");
-                        for (int i = 2; i <= rowCountLevels; i++)
-                        {
-                            excel.Range leveldata1 = excelWsLevels.Cells[i, 1];
-                            excel.Range leveldata2 = excelWsLevels.Cells[i, 2];
-
-                            string levelName = leveldata1.Value.ToString();
-                            double levelElev = leveldata2.Value;
-
-                            Level newLevel = Level.Create(doc, levelElev);
-                            newLevel.Name = levelName;
-                            newLevel.Elevation = levelElev;
-                        }
-
-
-                        FilteredElementCollector collector = new FilteredElementCollector(doc);
-                        collector.OfCategory(BuiltInCategory.OST_TitleBlocks);
-                        collector.WhereElementIsElementType();
-
-
-
-                        for (int j = 2; j <= rowCountSheets; j++)
-                        {
-                            excel.Range sheetdata1 = excelWsSheets.Cells[j, 1];
-                            excel.Range sheetdata2 = excelWsSheets.Cells[j, 2];
-
-                            string sheetNumber = sheetdata1.Value.ToString();
-                            string sheetName = sheetdata2.Value;
-
-                            ViewSheet newsheet = ViewSheet.Create(doc, collector.FirstElementId());
-                            newsheet.Name = sheetName;
-                            newsheet.SheetNumber = sheetNumber;
-
-                        }
-
-                        t.Commit();
-                        t.Dispose();
-
-
-                    }
-
-                    return Result.Succeeded;
+                    curVFT = curElem;
+                }
+                if(curElem.ViewFamily == ViewFamily.CeilingPlan)
+                {
+                    curRCPVFT = curElem;
                 }
             }
+
+
+
+            Level newLevel = Level.Create(doc, 100);
+            ViewPlan curPlan = ViewPlan.Create(doc, curVFT.Id, newLevel.Id);
+            ViewPlan curRCP = ViewPlan.Create(doc, curRCPVFT.Id, newLevel.Id);
+            curRCP.Name = curRCP.Name + " RCP";
+
+            ViewSheet viewSheet = ViewSheet.Create(doc, collecotr2.firstelementID);
+            Viewport viewPort = Viewport.Create(doc, newsheet.Id, new XYZ(0, 0, 0));
+
+            newSheet.Name = "Test Sheet";
+            newSheet.SheetNumber = "a10010101";
+
+
+            foreach(Parameter curParam in newSheet.Parameters)
+            {
+                if(curParam.Definition.Name == "Drawn By")
+                {
+                    curParam.Set("MM");
+                        
+                }
+            }
+
+            return Result.Succeeded;
+                }
+
+        internal View GetViewByName(Document dox, String viewName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(dox);
+            collector.OfClass(typof((View)));
+
+            foreach(View curView in collector)
+            {
+                if (curView.Name == viewName)
+            }
         }
+
+
+
+
+        internal struct TestStruct
+        {
+            public string Name;
+            public int Value;
+            public double Value2;
+            public TestStruct(string name, int value, double value2)
+            {
+                Name = name;
+                Value = value;
+                Value2 = value2;
+            }
+
+
+
+
+                 
+        }
+
+
     }
 }
 
