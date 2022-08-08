cdeb934bde5e2ac2e71177d985cc8611649ab61c
Eric Schoepfer <eric.schoepfer@gmail.com>
2022-08-08 13:37:11 -0700

    01

--- RevitAddinAcademy/App.cs
+++ RevitAddinAcademy/App.cs
@@ -9,6 +9,10 @@ using System.Diagnostics;
 using System.Reflection;
 using Autodesk.Revit.UI.Selection;
 
+
+
+
+
 #endregion
 
 namespace RevitAddinAcademy
@@ -21,7 +25,7 @@ namespace RevitAddinAcademy
 
                 try
                     {
-                    a.CreateRibbonTab("test Tab");
+                    a.CreateRibbonTab("Revit Add-in Academy");
                     }
                 catch (Exception)
                     {
@@ -30,13 +34,28 @@ namespace RevitAddinAcademy
 
             // Step 2:  Create Ribbon Panel
             //RibbonPanel curPanel = a.CreateRibbonPanel("Test Tab", "Test Panel");
-            RibbonPanel curPanel = CreateRibbonPanel(a, "Test Tab", "Test Panel");
+            RibbonPanel curPanel = CreateRibbonPanel(a, "Revit Add-in Academy", "Revit Tools");
 
             // Step 3:  Create Button Data instances
+            string assemblyName = GetAssemblyName();
+            PushButtonData pData1 = new PushButtonData("Tool1", "Tool 1", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData2 = new PushButtonData("Tool2", "Tool 2", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData3 = new PushButtonData("Tool3", "Tool 3", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData4 = new PushButtonData("Tool4", "Tool 4", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData5 = new PushButtonData("Tool5", "Tool 5", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData6 = new PushButtonData("Tool6", "Tool 6", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData7 = new PushButtonData("Tool7", "Tool 7", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData8 = new PushButtonData("Tool8", "Tool 8", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData9 = new PushButtonData("Tool9", "Tool 9", GetAssemblyName(), "RevitAddinAcademy.command");
+            PushButtonData pData10 = new PushButtonData("Tool10", "Tool 10", GetAssemblyName(), "RevitAddinAcademy.command");
+
+
+
+
 
 
             //  Step 4:  Add Images
-           
+
 
             //  Step 5:  Add tolltip Infor
 
@@ -47,23 +66,26 @@ namespace RevitAddinAcademy
 
 
 
-            
-            
-            
-            
-            
+
+
+
+
+
             return Result.Succeeded;
         }
+
+        private string GetAssemblyName()
+        {
+            return Assembly.GetExecutingAssembly().Location;
+        }
+
+
 
         private RibbonPanel CreateRibbonPanel(UIControlledApplication a,
                                               string tabName,
                                               string panelName)
         {
-            if (tabName is null)
-            {
-                throw new ArgumentNullException(nameof(tabName));
-            }
-
+            
             foreach (RibbonPanel tmpPanel in a.GetRibbonPanels(tabName))
                 {
                     if(tmpPanel.Name == panelName)
